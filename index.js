31c08fd3cd8b74b98ada057ca3b7f4ca4bb15f2b
vishal kumar <136618054+VishalKumarddu@users.noreply.github.com>
2024-09-04 00:07:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let originalArr = [1,2,3,4,5,6]
+let reverseArr =[]
+
+let reversedArr =()=>{
+
+  for (let i= originalArr.length -1; i>=0; i--){
+    reverseArr.push(originalArr[i])
+  }
+  console.log(reverseArr)
+}
+reversedArr()
+
+
+
